df6e81d9d8fc8b58e75bc1fd638469be767353bf
Colby Morris <ccolbymorris@gmail.com>
2016-08-19 11:37:50 -0500

    Added Facebook login button

--- app/src/main/java/com/example/zcolbeast/fragprac/MainActivity.java
+++ app/src/main/java/com/example/zcolbeast/fragprac/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.zcolbeast.fragprac;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+        AppEventsLogger.activateApp(getApplication());
     }
 }
--- app/src/main/java/com/example/zcolbeast/fragprac/room_listing.java
+++ app/src/main/java/com/example/zcolbeast/fragprac/room_listing.java
@@ -6,22 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 /**
- * Created by zColbeast on 8/15/2016.
+ * Created by zColbeast on 8/18/2016.
  */
-public class Frag_One extends Fragment {
+public class room_listing extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.frag_one, container, false);
-
-
-
-        return view;
+        return inflater.inflate(R.layout.room_listing, container, false);
     }
 }
